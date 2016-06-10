6fb422070dab375e27481ad70899abb6fd0e686b
Oscar Lim <olim@ucla.edu>
2016-06-10 15:05:31 -0700

    Fix githead.py to work with packed refs

--- build/util/githead.py
+++ build/util/githead.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python
+import os
 import re
 from sys import argv
 
+dirname = os.path.dirname(argv[1])
 f = open(argv[1], 'r')
 head = f.readline()
 f.close()
 match = re.match('ref: (.*)', head)
-if match:
+if match and os.path.isfile(dirname + "/" + match.group(1)):
   print match.group(1)
+elif match:
+  print "packed-refs"
 else:
   print ""
